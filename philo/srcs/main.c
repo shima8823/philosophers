3ae8cc241e39b76c04136494c3b85701199cf467
shima8823 <taiyaki1275@gmail.com>
2022-08-28 09:38:03 +0900

    initial commit

---
+++ philo/srcs/main.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 10:39:50 by shima             #+#    #+#             */
+/*   Updated: 2022/08/27 11:16:09 by shima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	main(int argc, char *argv[])
+{
+	
+}
+
+// void *do_something(void *arg)
+// {
+//    printf("do something\n");
+//    sleep(1);
+// }
+
+// int main(void)
+// {
+//    pthread_t thread;
+//    void *retval;
+
+//    // スレッドを作成
+//    if (pthread_create(&thread, NULL, do_something, NULL) != 0)
+//        // エラー処理
+//        return (0);
+//    // スレッドの終了方法
+//    // 1. スレッドが終了するまで待つ
+//    pthread_join(thread, &retval);
+//    // 2. スレッドを待たない
+//    pthread_detach(thread);
+// }
