87029be92f1ae87c606e29ea2db6299ce40675af
shima8823 <taiyaki1275@gmail.com>
2022-09-01 12:02:48 +0900

    add: init_monitor and init_philo

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:40:17 by shima             #+#    #+#             */
-/*   Updated: 2022/08/28 09:57:25 by shima            ###   ########.fr       */
+/*   Updated: 2022/09/01 11:55:44 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,27 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-typedef struct s_arg
+typedef struct s_philo
 {
-	long number_of_philosophers;
-	int	time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-}	t_arg ;
+	int	id;
+	int right;
+	int left;
+	
+
+}	t_philo;
+
+typedef struct s_monitor
+{
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	pthread_t		*ptherads;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+}	t_monitor;
+
+
 
 
 int	ft_atoi(const char *str);
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,43 +6,88 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:39:50 by shima             #+#    #+#             */
-/*   Updated: 2022/08/28 10:56:09 by shima            ###   ########.fr       */
+/*   Updated: 2022/09/01 12:02:07 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-// int	main(int argc, char *argv[])
-// {
-// 	t_arg	info;
+bool	init_monitor(int argc, char *argv[], t_monitor *monitor);
+bool	init_philo(t_monitor *monitor, t_philo **philo);
+void	*philosophers(void *arg);
 
-// 	info.number_of_philosophers = ft_atoi(argv[1]);
-// 	info.number_of_philosophers = ft_atoi(argv[1]);
-// }
-
-void *f(void *p)
+int	main(int argc, char *argv[])
 {
-	int	*cnt;
+	t_monitor	monitor;
+	t_philo		*philo;
+	int			i;
 
-	cnt = p;
-	for (int i = 0; i < 10000; ++i)
-		++(*cnt);
-	return (NULL);
+	
+	init_monitor(argc, argv, &monitor);
+	printf("%d", monitor.number_of_philosophers);
+	init_philo(&monitor, &philo);
+	monitor.philos = philo;
+	
+	i = 0;
+	while (i < monitor.number_of_philosophers)
+	{
+		pthread_create(&monitor.ptherads[i], NULL, philosophers, &monitor);
+		i++;
+	}
+
+	return (0);
 }
 
-int main()
+bool init_monitor(int argc, char *argv[], t_monitor *monitor)
 {
-	int cnt = 0;
-	pthread_t thread1;
-	pthread_t thread2;
+	int i;
+	if (argc != 5 && argc != 6)
+		return (false);
+	monitor->number_of_philosophers = ft_atoi(argv[1]);
+	monitor->time_to_die = ft_atoi(argv[2]);
+	monitor->time_to_eat = ft_atoi(argv[3]);
+	monitor->time_to_sleep = ft_atoi(argv[4]);
+	monitor->forks = malloc(sizeof(pthread_mutex_t *) * monitor->number_of_philosophers);
+	monitor->ptherads = malloc(sizeof(pthread_t *) * monitor->number_of_philosophers);
 
-	// mainの処理とは別に働かせる。並列処理。
-	pthread_create(&thread1, NULL, &f, &cnt);
-	pthread_create(&thread2, NULL, &f, &cnt);
+	i = 0;
+	while (i < monitor->number_of_philosophers)
+	{
+		pthread_mutex_init(&(monitor->forks[i]), NULL);
+		i++;
+	}
+	return (true);
+}
 
-	// 終了するまで待機。pthread_createとセット
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
+bool init_philo(t_monitor *monitor, t_philo **philo)
+{
+	int	i;
+	
+	philo = malloc(sizeof(t_philo *) * monitor->number_of_philosophers);
+	if (!philo)
+		return (false);
+	while (i < monitor->number_of_philosophers)
+	{
+		philo[i] = malloc(sizeof(t_philo));
+		if (!(philo[i]))
+			return (false);
+		philo[i]->id = i + 1;
+		philo[i]->right = i;
+		philo[i]->left = i + 1;
 
-	printf("cnt: %d\n", cnt); // 20000がでると思った？
+		if (philo[i]->left == monitor->number_of_philosophers)
+			philo[i]->left = 0;
+		
+		
+		i++;
+	}
+	return (true);
+}
+
+void	*philosophers(void *arg)
+{
+	t_monitor *monitor;
+
+	monitor = arg;
+	return (NULL);
 }
