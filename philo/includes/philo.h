9f0b58daebaa06bc3da533b32525d01698424448
shima8823 <taiyaki1275@gmail.com>
2022-09-05 19:03:36 +0900

    Add: Functions that philosophers act on

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:40:17 by shima             #+#    #+#             */
-/*   Updated: 2022/09/01 11:55:44 by shima            ###   ########.fr       */
+/*   Updated: 2022/09/05 10:55:14 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,27 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <sys/time.h>
 
 typedef struct s_philo
 {
 	int	id;
 	int right;
 	int left;
+	long long time_last_meal;
 	
-
+	pthread_t		thread;
+	struct s_monitor	*monitor;
 }	t_philo;
 
 typedef struct s_monitor
 {
-	int				number_of_philosophers;
+	int				num_of_philos;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
-	pthread_t		*ptherads;
+	
+	
 	pthread_mutex_t	*forks;
 	t_philo			*philos;
 }	t_monitor;
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,32 +6,49 @@
 /*   By: shima <shima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/27 10:39:50 by shima             #+#    #+#             */
-/*   Updated: 2022/09/01 12:02:07 by shima            ###   ########.fr       */
+/*   Updated: 2022/09/05 11:46:18 by shima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
 bool	init_monitor(int argc, char *argv[], t_monitor *monitor);
-bool	init_philo(t_monitor *monitor, t_philo **philo);
+bool	init_philo(t_monitor *monitor);
 void	*philosophers(void *arg);
+long long	get_timestamp(void);
+
+void	grab_fork(t_monitor *monitor, int id, int index);
+void	eating(int id, long long *time_last_meal, int time_to_eat);
+void	down_forks(t_monitor *monitor, int right, int left);
+void	sleeping(int time_to_sleep, int id);
+void	thinking(int id);
+
+
+// test
+void	print_monitor(t_monitor monitor);
 
 int	main(int argc, char *argv[])
 {
 	t_monitor	monitor;
-	t_philo		*philo;
 	int			i;
 
 	
-	init_monitor(argc, argv, &monitor);
-	printf("%d", monitor.number_of_philosophers);
-	init_philo(&monitor, &philo);
-	monitor.philos = philo;
+	if (!init_monitor(argc, argv, &monitor))
+		return (1);
+	init_philo(&monitor);
+	print_monitor(monitor);
 	
 	i = 0;
-	while (i < monitor.number_of_philosophers)
+	while (i < monitor.num_of_philos)
 	{
-		pthread_create(&monitor.ptherads[i], NULL, philosophers, &monitor);
+		pthread_create(&monitor.philos[i].thread, NULL, philosophers, &(monitor.philos[i]));
+		i++;
+	}
+
+	i = 0;
+	while (i < monitor.num_of_philos)
+	{
+		pthread_join(monitor.philos[i].thread, NULL);
 		i++;
 	}
 
@@ -43,15 +60,17 @@ bool init_monitor(int argc, char *argv[], t_monitor *monitor)
 	int i;
 	if (argc != 5 && argc != 6)
 		return (false);
-	monitor->number_of_philosophers = ft_atoi(argv[1]);
+	// printf("%s, %d\n", __FILE__, __LINE__);
+	monitor->num_of_philos = ft_atoi(argv[1]);
 	monitor->time_to_die = ft_atoi(argv[2]);
 	monitor->time_to_eat = ft_atoi(argv[3]);
 	monitor->time_to_sleep = ft_atoi(argv[4]);
-	monitor->forks = malloc(sizeof(pthread_mutex_t *) * monitor->number_of_philosophers);
-	monitor->ptherads = malloc(sizeof(pthread_t *) * monitor->number_of_philosophers);
+	monitor->forks = malloc(sizeof(pthread_mutex_t) * monitor->num_of_philos);
+	if (!monitor->forks)
+		return (false);
 
 	i = 0;
-	while (i < monitor->number_of_philosophers)
+	while (i < monitor->num_of_philos)
 	{
 		pthread_mutex_init(&(monitor->forks[i]), NULL);
 		i++;
@@ -59,26 +78,22 @@ bool init_monitor(int argc, char *argv[], t_monitor *monitor)
 	return (true);
 }
 
-bool init_philo(t_monitor *monitor, t_philo **philo)
+bool init_philo(t_monitor *monitor)
 {
 	int	i;
 	
-	philo = malloc(sizeof(t_philo *) * monitor->number_of_philosophers);
-	if (!philo)
+	monitor->philos = malloc(sizeof(t_philo) * monitor->num_of_philos);
+	if (!(monitor->philos))
 		return (false);
-	while (i < monitor->number_of_philosophers)
+	i = 0;
+	while (i < monitor->num_of_philos)
 	{
-		philo[i] = malloc(sizeof(t_philo));
-		if (!(philo[i]))
-			return (false);
-		philo[i]->id = i + 1;
-		philo[i]->right = i;
-		philo[i]->left = i + 1;
-
-		if (philo[i]->left == monitor->number_of_philosophers)
-			philo[i]->left = 0;
-		
-		
+		monitor->philos[i].id = i + 1;
+		monitor->philos[i].right = i;
+		monitor->philos[i].left = i + 1;
+		if (monitor->philos[i].left == monitor->num_of_philos)
+			monitor->philos[i].left = 0;
+		monitor->philos[i].monitor = monitor;
 		i++;
 	}
 	return (true);
@@ -86,8 +101,88 @@ bool init_philo(t_monitor *monitor, t_philo **philo)
 
 void	*philosophers(void *arg)
 {
+	t_philo *philo;
 	t_monitor *monitor;
 
-	monitor = arg;
+	philo = arg;
+	monitor = philo->monitor;
+	if (philo->id % 2 == 1)
+		usleep(200);
+	grab_fork(philo->monitor, philo->id, philo->right);
+	grab_fork(philo->monitor, philo->id, philo->left);
+// eating
+	eating(philo->id, &(philo->time_last_meal), monitor->time_to_eat);
+// down fork
+	down_forks(monitor, philo->right, philo->left);
+// sleep
+	sleeping(monitor->time_to_sleep, philo->id);
+// thinking
+	thinking(philo->id);
+
+
 	return (NULL);
+}
+
+void	grab_fork(t_monitor *monitor, int id, int index)
+{
+	pthread_mutex_lock(&(monitor->forks[index]));
+	printf("%lld %d has taken a fork\n", get_timestamp(), id);
+}
+
+void	eating(int id, long long *time_last_meal, int time_to_eat)
+{
+	*time_last_meal = get_timestamp();
+	printf("%lld %d is eating\n", *time_last_meal, id);
+	while (get_timestamp() - *time_last_meal < time_to_eat)
+		usleep(1 * 1000);
+}
+
+void	down_forks(t_monitor *monitor, int right, int left)
+{
+	pthread_mutex_unlock(&(monitor->forks[right]));
+	pthread_mutex_unlock(&(monitor->forks[left]));
+}
+
+void	sleeping(int time_to_sleep, int id)
+{
+	long long	time_start;
+
+	time_start = get_timestamp();
+	printf("%lld %d is sleeping\n", time_start, id);
+	while (get_timestamp() - time_start < time_to_sleep)
+		usleep(1 * 1000);
+}
+
+void	thinking(int id)
+{
+	printf("%lld %d is is thinking\n", get_timestamp(), id);
+}
+
+long long	get_timestamp(void)
+{
+	struct timeval	tv;
+	if (gettimeofday(&tv, NULL) == -1)
+		return (-1);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+// test
+
+void	print_monitor(t_monitor monitor)
+{
+	int i;
+
+	printf("num_of_philo: %d\n", monitor.num_of_philos);
+	printf("time_to_die: %d\n", monitor.time_to_die);
+	printf("time_to_eat: %d\n", monitor.time_to_eat);
+	printf("time_to_sleep: %d\n", monitor.time_to_sleep);
+	i = 0;
+	while (i < monitor.num_of_philos)
+	{
+		printf("philo[%d].id: %d\n", i, monitor.philos[i].id);
+		printf("philo[%d].right: %d\n", i, monitor.philos[i].right);
+		printf("philo[%d].left: %d\n", i, monitor.philos[i].left);
+		printf("---------------------------\n");
+		i++;
+	}
 }
